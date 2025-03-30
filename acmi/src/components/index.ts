9d371e3904368bc74b935138c519fb7c7e676c93
Andrey Teslenko <andteslenko@gmail.com>
2025-03-30 22:43:28 +0300

    created card

--- acmi/src/components/index.ts
+++ acmi/src/components/index.ts
@@ -2,6 +2,9 @@ export { Button } from './button';
 export { Checkbox } from './checkbox';
 export { Input } from './input';
 export { InfiniteScrollList } from './infinite-scroll-list';
+export { SuggestionCard } from './suggestion-card';
+
+export type { AircraftProps } from './suggestion-card';
 
 export * from './selects';
 export * from './switchers';
