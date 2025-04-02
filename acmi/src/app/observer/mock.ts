e6734e13973519387b1104630441e61d918fb144
Andrey Teslenko <andteslenko@gmail.com>
2025-04-02 22:38:00 +0300

    fix_card_finance: Added intl function

--- acmi/src/app/observer/mock.ts
+++ acmi/src/app/observer/mock.ts
@@ -6,7 +6,7 @@ export const mockAircrafts = Array.from({ length: 100 }, (_, index) => ({
   registration: `GG-WOW-${index + 1}`,
   dom: `01/${(index % 12) + 1}/201${index % 10}`, // Варьируем месяц и год
   layout: `${160 + (index % 20)}Y+${5 + (index % 10)}J`,
-  bhPrice: `$${(2000 + index * 50).toLocaleString()}`,
-  indicativePrice: `$${(180000 + index * 1000).toLocaleString()}`,
+  bhPrice: `${2000 + index * 50}`,
+  indicativePrice: `${180000 + index * 1000}`,
   imageUrl: `https://s3-alpha-sig.figma.com/img/09cc/1f86/3422898ca4e0587df2b2d5589a0c2e3e?Expires=1743984000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=LCMXh4zult0-lZsovWTjNo7--z2w1lfX6eWnZmbriquJW~xGTJqaiX9pCzrkGfzMluzBHql4-8AArO6SdNdzUe4QLMKQ-1Ln6EyG36-porCUtFsnRypHxBpIQgHWLpK8PP4lZ8Gw23DdiHbpaD-4xgN-fHRLuykSZ49iZJkJFMhAMuzJFw5TXpyyWn6ugczs9Pe9nnT7mnb0BeYWjZIPFcOaPx1zM4X67yBX5Cs~Chkxs~qaKiyZdo8UxvvtS-GFGAj1rsvh10hWDhcVXPuw06jCP8D4hEoWgRqVYtYGHSrFu3pelqp80ktmd1srI9YOiJC53R~pIabuMXstdBxJYQ__`,
 }));
