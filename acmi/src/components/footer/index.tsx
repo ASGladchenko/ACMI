f417494d4c1771a93d245a80592e5cbbc8269946
ASGladchenko <asgladchenko@gmail.com>
2025-04-03 22:57:08 +0300

    finish footer adaptive

--- acmi/src/components/footer/index.tsx
+++ acmi/src/components/footer/index.tsx
@@ -4,36 +4,38 @@ import { ClientLink } from '../links';
 export const Footer = ({}) => {
   return (
     <footer className="bg-white-dark">
-      <div className="mx-auto flex w-full max-w-[1310px] flex-col items-center px-[35px] py-9">
+      <div className="laptop:px-[35px] mx-auto flex w-full max-w-[1310px] flex-col items-center px-5 py-9">
         <div className="border-b-gray-medium flex w-full items-center justify-between border-b pb-9">
-          <nav className="flex items-center gap-10">
-            <ClientLink href="/">Home</ClientLink>
+          <nav className="laptop:flex-row laptop:gap-10 flex max-w-[50%-10px] flex-col items-start">
+            <ClientLink className="text-sm min-[420px]:text-base" href="/">
+              Home
+            </ClientLink>
 
-            <ClientLink isDisabled href="/">
+            <ClientLink className="text-sm min-[420px]:text-base" isDisabled href="/">
               Provider Dashboard
             </ClientLink>
 
-            <ClientLink isDisabled href="/">
+            <ClientLink className="text-sm min-[420px]:text-base" isDisabled href="/">
               Customer Dashboard
             </ClientLink>
           </nav>
 
-          <nav className="flex items-center gap-10">
-            <ClientLink isDisabled href="/">
+          <nav className="laptop:flex-row laptop:gap-10 flex max-w-[50%-10px] flex-col items-end">
+            <ClientLink className="text-sm min-[420px]:text-base" isDisabled href="/">
               Terms of service
             </ClientLink>
 
-            <ClientLink isDisabled href="/">
+            <ClientLink className="text-sm min-[420px]:text-base" isDisabled href="/">
               Contacts
             </ClientLink>
 
-            <ClientLink isDisabled href="/">
+            <ClientLink className="text-sm min-[420px]:text-base" isDisabled href="/">
               Privacy policy
             </ClientLink>
           </nav>
         </div>
 
-        <div className="flex w-full items-center justify-between pt-9">
+        <div className="flex w-full flex-col items-center justify-between gap-3 pt-9 min-[420px]:flex-row min-[420px]:gap-1">
           <Logo size="m" />
 
           <p className="text-gray-dark font-medium">
