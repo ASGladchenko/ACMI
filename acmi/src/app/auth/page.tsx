8627be72715d39d2c2021d546d265c6d21693d36
ASGladchenko <asgladchenko@gmail.com>
2025-05-10 22:17:35 +0300

    sort

--- acmi/src/app/auth/page.tsx
+++ acmi/src/app/auth/page.tsx
@@ -1,7 +1,7 @@
 export default async function AuthPage() {
   return (
     <span className="text-center text-[50px] text-black">
-      The application is temporarily unavailable
+      The application is temporarily unavailable yet
     </span>
   );
 }
