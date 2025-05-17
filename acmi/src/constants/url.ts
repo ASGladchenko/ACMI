3bacb9f0c153621cc95ccc8a06e2351a79e56c89
ASGladchenko <asgladchenko@gmail.com>
2025-05-17 15:24:47 +0300

    changes api

--- acmi/src/constants/url.ts
+++ acmi/src/constants/url.ts
@@ -1,15 +1,17 @@
-const isLocal = process.env.NEXT_PUBLIC_IS_LOCAL === 'true';
+export const baseApiUrl = process.env.NEXT_PUBLIC_API_URL!;
+export const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI!;
 
-export const baseApiUrl = isLocal ? 'http://localhost:8080' : 'https://api.acmi.direct';
+export const authUrl =
+  `https://acmidirect.b2clogin.com/acmidirect.onmicrosoft.com/b2c_1_login/oauth2/v2.0/authorize?` +
+  `client_id=e99b74b5-b7e7-4311-8349-2d228678065a&` +
+  `response_type=code&` +
+  `redirect_uri=${redirectUri}&` +
+  `scope=openid&` +
+  `state=12345&` +
+  `nonce=test123`;
 
-const redirectUri = isLocal
-  ? 'http://localhost:8080/auth/callback'
-  : 'https://api.acmi.direct/auth/callback';
-
-export const authUrl = `https://acmidirect.b2clogin.com/acmidirect.onmicrosoft.com/b2c_1_login/oauth2/v2.0/authorize?
-&client_id=e99b74b5-b7e7-4311-8349-2d228678065a&
-response_type=code&
-redirect_uri=${redirectUri}&
-scope=openid&
-state=12345&
-nonce=test123`;
+console.log({
+  baseApiUrl,
+  redirectUri,
+  authUrl,
+});
