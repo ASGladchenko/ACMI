1848f0c3749be8cf23c32b9720bcea94ea96a27b
ASGladchenko <asgladchenko@gmail.com>
2025-10-03 16:21:00 +0300

    check iframe src

--- acmi/src/app/(divider)/(dashboard)/dashboard-customer/contracts/[id]/page.tsx
+++ acmi/src/app/(divider)/(dashboard)/dashboard-customer/contracts/[id]/page.tsx
@@ -1,35 +1,27 @@
-'use client';
-
 import { apiServer } from '@/fetch-request';
 import { getErrorMessage } from '@/utils';
-import { useEffect } from 'react';
 
-export default function ContractPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function ContractPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   let errors;
   let cfi = { HostEditUrl: '' };
 
-  const getIframe = async () => {
-    try {
-      await apiServer
-        .get(`/rfq/contract`, { params: { rfq_id: id } })
-        .then(({ data }) => (cfi = data));
-    } catch (error) {
-      errors = getErrorMessage(error, 'Failed to load contract info, connect support please');
-    }
-  };
+  try {
+    await apiServer
+      .get(`/rfq/contract`, { params: { rfq_id: id } })
+      .then(({ data }) => (cfi = data));
+  } catch (error) {
+    errors = getErrorMessage(error, 'Failed to load contract info, connect support please');
+  }
 
   const base = 'https://collabora.acmi.direct/browser/dist/cool.html';
   const iframeSrc = base + cfi?.HostEditUrl;
 
   console.log({ cfi, errors, iframeSrc });
 
-  useEffect(() => {
-    getIframe();
-  }, []);
-
   return (
     <div className="laptop:px-0 laptop:pr-1 h-[calc(100vh-120px)] w-full px-2">
+      <p>{iframeSrc}</p>
       <iframe
         src={iframeSrc}
         allowFullScreen
