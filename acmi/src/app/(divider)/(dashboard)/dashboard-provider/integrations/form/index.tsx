997fda3efd5d569bc4d837a1675f4c6a4d4f54c5
Anatoliy Hladchenko <105244016+ASGladchenko@users.noreply.github.com>
2025-11-27 22:32:59 +0200

    Leon intagration (#50)
    * create form integration
    
    * connect change leon's data and redirect

---
+++ acmi/src/app/(divider)/(dashboard)/dashboard-provider/integrations/form/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Input, Button } from '@/components';
+
+import { useLeonConnect } from './api';
+
+export const IntegrationsForm = ({}) => {
+  const [id, setId] = useState('');
+
+  const { isLoading, connect } = useLeonConnect();
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Input label="Enter your Leon`s id " value={id} onChange={(value) => setId(value)} />
+
+      <Button disabled={!id} loading={isLoading} onClick={() => connect(id)}>
+        Connect
+      </Button>
+    </div>
+  );
+};
